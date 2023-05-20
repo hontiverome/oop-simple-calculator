dff8c4799be4335c49af86ecdc9ede30f8a20316
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 23:13:48 +0800

    comments

--- ui_class.py
+++ ui_class.py
@@ -4,27 +4,32 @@ class UI():
     # initializes variables
     def __init__(self, operation) -> None:
         self.arithmetic_operation=operation
-      
+    
+    # registers addition unto variable operation
     def intro_add(self):
         print("\nYou chose addition")
         operation = str('Addition')
         self.arithmetic_operation = operation
-
+    
+    # registers subtraction unto variable operation
     def intro_sub(self):
         print("\nYou chose subtraction")
         operation = str('Subtraction')
         self.arithmetic_operation = operation
 
+    # registers multiplication unto variable operation
     def intro_mul(self):
         print("\nYou chose multiplication")
         operation = str('Multiplication')
         self.arithmetic_operation = operation
 
+    # registers division unto variable operation
     def intro_div(self):
         print("\nYou chose division")
         operation = str('Division')
         self.arithmetic_operation = operation 
 
+    # method to print results in an animated way
     def animated(self, number1,number2, result):
         import time
         print('\nOperation')
@@ -38,6 +43,7 @@ class UI():
         for i in range(len(animation)):
             print(animation[i], end='', flush=True)
             time.sleep(1.5)
+
         # Print the Result
         print(f"\n\nHere is your result\n:{result}")
 
