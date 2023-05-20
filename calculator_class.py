63232f79f8f0af117f96ab538382d22c5df89b13
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 23:02:13 +0800

    added comments

--- calculator_class.py
+++ calculator_class.py
@@ -1,29 +1,36 @@
 # Calculator class
 from ui_class import UI
 class Calculator:
+    # initializes variables
     def __init__(self, firstN, secondN, result) -> None:
         self.number1=firstN 
         self.number2=secondN
         self.results=result
-        
+    
+    # registers input    
     def inputNum1(self, firstN):
         self.number1=float(firstN)
-
+        
+    # registers input   
     def inputNum2(self, secondN):
         self.number2=float(secondN)
     
+    # adds inputs
     def addition(self):
         result=self.number1+self.number2
         self.results=float(result)
     
+    # subtracts inputs
     def subtraction(self):
         result=self.number1-self.number2
         self.results=float(result)
     
+    # multiplies inputs
     def multiplication(self):
         result=self.number1*self.number2
         self.results=float(result)
     
+    # divides inputs
     def division(self):
         result=self.number1/self.number2
         self.results=float(result)
