912a4a9d33212fbf4c86c3c2f3d0885287d95333
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 21:19:41 +0800

    animated ui test

--- ui_class.py
+++ ui_class.py
@@ -3,23 +3,38 @@
 class UI:
     def __init__(self) -> None:
         print("Welcome to the Calculator!")
-        self.operation=operation
-        
+        self.operation = operation
+
     def start():
         print("Welcome to the Calculator!")
-    
+
     def intro_add(self):
         print("\nYou chose addition")
-        operation='Addition'
+        operation = 'Addition'
 
     def intro_sub(self):
         print("\nYou chose subtraction")
-        operation='Subtraction'
-        
+        operation = 'Subtraction'
+
     def intro_mul(self):
         print("\nYou chose subtraction")
-        operation='Subtraction'
+        operation = 'Subtraction'
 
     def intro_div(self,):
         print("\nYou chose subtraction")
-        operation='Subtraction'
+        operation = 'Subtraction'
+
+    def animated(self):
+        print('\nOperation')
+        time.sleep(1)
+        print(f': {self.operation}')
+        time.sleep(1.5)
+        print(f"Numbers to be computed")
+        time.sleep(1)
+        print(f': {firstN, secondN}')
+        animation = '...'
+        for i in range(len(animation)):
+            print(animation[i], end='', flush=True)
+            time.sleep(1.5)
+        # Print the Result
+        print(f"\n\nHere is your result\n:{result}")
