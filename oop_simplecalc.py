0bb3874160912e4d84a4b51ab9ee8a329998bf6e
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 22:01:13 +0800

    working print output

--- oop_simplecalc.py
+++ oop_simplecalc.py
@@ -2,30 +2,45 @@
 from ui_class import UI
 from calculator_class import Calculator
 
+
 def SimpleCalculator():
-    calculate=Calculator(1,1)
-    userInterface=UI
+    calculate = Calculator(1, 1, 1)
+    userInterface = UI
     userInterface.start()
     while True:
-        user_choice=str(input("\nType the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
-        if user_choice=='+':
+        user_choice = str(input(
+            "\nType the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
+        if user_choice == '+':
             userInterface.intro_add()
             break
-        elif user_choice=='-':
+        elif user_choice == '-':
             userInterface.intro_sub()
             break
-        elif user_choice=='*':
+        elif user_choice == '*':
             userInterface.intro_mul()
             break
-        elif user_choice=='/':
+        elif user_choice == '/':
             userInterface.intro_div()
             break
         else:
             print("Invalid.")
-            
-    firstN=float(input("\nWhat is your first number?\n"))
+        return user_choice
+
+    # Takes input and stores it into variables
+    firstN = float(input("\nWhat is your first number?\n"))
     calculate.inputNum1(firstN)
-    secondN=float(input("\nWhat is your second number?\n"))
+    secondN = float(input("\nWhat is your second number?\n"))
     calculate.inputNum2(secondN)
-        
+
+    if user_choice == '+':
+        calculate.addition()
+    elif user_choice == '-':
+        calculate.subtraction()
+    elif user_choice == '*':
+        calculate.multiplication()
+    elif user_choice == '/':
+        calculate.division()
+
+    print(calculate.results)
+
 SimpleCalculator()
