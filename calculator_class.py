111b26af43e23647ac6890351b939b1644b1a351
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 21:51:20 +0800

    arithmetic operation methods

--- calculator_class.py
+++ calculator_class.py
@@ -10,4 +10,19 @@ class Calculator:
 
     def inputNum2(self, secondN):
         self.number2-float(secondN)
-        
+    
+    def addition(self):
+        sum=self.number1+self.number2
+        return sum
+    
+    def subtraction(self):
+        difference=self.number1-self.number2
+        return difference
+    
+    def multiplication(self):
+        product=self.number1*self.number2
+        return product
+    
+    def division(self):
+        quotient=self.number1/self.number2
+        return quotient
