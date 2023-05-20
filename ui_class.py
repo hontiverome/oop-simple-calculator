f5a308f45efb31d4640272e8944925813cd1f416
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 21:03:10 +0800

    modified start method

--- ui_class.py
+++ ui_class.py
@@ -3,24 +3,7 @@
 class UI:
     def __init__(self) -> None:
         print("Welcome to the Calculator!")
+        self.choice = user_choice
         
-    def start(self, user_choice):
-        while True:
-            user_choice = str(input("\nType the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
-            if user_choice == '+':
-                print("\nYou chose addition")
-                operation='Addition'
-            elif user_choice == '-':
-                print("\nYou chose subtraction")
-                operation='Subtraction'
-            elif user_choice == '*':
-                print("\nYou chose multiplication")
-                operation='Multiplication'
-            elif user_choice == '/':
-                print("\nYou chose division")
-                operation='Division'
-            else:
-                print("Invalid.\n")
-                continue
-            return operation
-    
+    def start(self):
+        print("Welcome to the Calculator!")
