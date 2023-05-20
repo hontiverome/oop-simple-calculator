2b69be599d301b9683fbc5b2faf9b18ad7fd4caf
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 21:29:35 +0800

    test

--- calculator_class.py
+++ calculator_class.py
@@ -1,5 +1,8 @@
 # Calculator class
 from ui_class import UI
 class Calculator:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, firstN, secondN) -> None:
+        self.number1=firstN 
+        self.number2=secondN
+    
+    def 
