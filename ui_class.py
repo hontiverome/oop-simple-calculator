8f11979aeb6d3218a9a13f1464942f3eaa5504ab
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 21:34:02 +0800

    removed unwanted initialization in ui

--- ui_class.py
+++ ui_class.py
@@ -1,9 +1,6 @@
 # UI class
 # creates class
 class UI:
-    def __init__(self) -> None:
-        print("Welcome to the Calculator!")
-
     def start():
         print("Welcome to the Calculator!")
 
