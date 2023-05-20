805c1b2f24f71c45820f8459fbb0f415ef5116fe
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-20 21:46:17 +0800

    changed name for ui

--- oop_simplecalc.py
+++ oop_simplecalc.py
@@ -3,20 +3,29 @@ from ui_class import UI
 from calculator_class import Calculator
 
 def SimpleCalculator():
-    calculate=Calculator
-    ui=UI
-    ui.start()
+    calculate=Calculator(1,1)
+    userInterface=UI
+    userInterface.start()
     while True:
         user_choice=str(input("\nType the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
         if user_choice=='+':
-            ui.intro_add()
+            userInterface.intro_add()
+            break
         elif user_choice=='-':
-            ui.intro_sub()
+            userInterface.intro_sub()
+            break
         elif user_choice=='*':
-            ui.intro_mul()
+            userInterface.intro_mul()
+            break
         elif user_choice=='/':
-            ui.intro_div()
+            userInterface.intro_div()
+            break
         else:
             print("Invalid.")
             
+    firstN=float(input("\nWhat is your first number?\n"))
+    calculate.inputNum1(firstN)
+    secondN=float(input("\nWhat is your second number?\n"))
+    calculate.inputNum2(secondN)
+        
 SimpleCalculator()
